6b525344d0fcb65fa8af9b63bb6cefccac9c1143
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-22 20:46:49 +0530

    commit input  eval

---
+++ eval.py
@@ -0,0 +1,14 @@
+#eval it return by return user define data type
+# expretion evalution
+#y=eval(input("enter any number"))
+
+#print(y,type(y))
+
+
+x=10
+y='java'
+
+#z=eval('x+y')
+#print(z)
+
+print(type(eval('x+y')))
