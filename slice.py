3ccf6bac0a58327396329774f16158b6cdd0727a
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-25 20:53:30 +0530

    slice

---
+++ slice.py
@@ -0,0 +1,20 @@
+
+
+str=input("Enter any string")
+
+print(str[::-1])
+
+s=input("Enter your name :")
+# x=slice(None,None,-1)
+# print(s[x])
+
+# print(s[slice(None,None,-1)])
+
+# rules of slice = 1.find out step direction . if step is not given it take by default step
+# direction is positive
+# 2. find out start - stop step direction.but if any of start or stop orboth of them not given
+# them not given them it follow step direction.
+# 3. it above
+
+
+# print(s[::-1])
