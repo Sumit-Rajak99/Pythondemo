bfa1fe582ff9b85c7e41715903a44ca89df1701c
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-26 21:01:04 +0530

    rangequestion2

---
+++ indslicranquestion.py
@@ -0,0 +1,6 @@
+
+#index
+s='pyhon'
+#print(s.index('o'))
+
+print(s.index('h',-1-6))
---
+++ range.py
@@ -0,0 +1,48 @@
+# # synax=>range(start,stop,step/direction)
+# # range(sart,stop)
+# # range(stop)
+# #x=range(5)
+# #print(list(x))
+# #print(tuple(x))
+
+
+# # x=(1,2,3,4,5,6,7,8,9,10)
+# #x=range(1,11,1)
+# #x=range(2,11,2)
+# #x=range(1,10,2)
+# #x=range(-1,-11,-1)
+# #x=range(-2,-11,-2)
+# ''x=range(-1,-10,-2)
+# print(list(x))
+
+#x=range(1,10,-1)
+#x=range(5,5,)
+#print(list(x))
+
+x=range(3,-3,-2)
+print(tuple(x))
+
+x=range(-3,3)
+print(list(x))
+
+
+
+
+#wap to create collection of n nature number
+# n=int(input("enter any number:"))
+# print(list(range(1,n+1))) 
+# print(tuple(range(2,int(input("enter any number"))+1,2)))
+
+
+#wap to create collection of  up to n even number
+# print(tuple(range(2,int(input("enter any number"))+1,2)))
+
+
+#wap  two addition number
+
+# x=float(input("enter number1:"))
+# y=float(input("enter number2:"))
+# z=x+y
+# print(z)
+
+print(int(input("enter number1:"))+int(input("enter number2:")))
