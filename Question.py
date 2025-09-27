60d14ec78ae5d677b7f6fbb2aa83be5c99e20f42
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-27 22:37:49 +0530

    questionrangeslice

---
+++ Question.py
@@ -0,0 +1,31 @@
+
+
+s='python language'
+
+# print(s.index(' '))  right
+
+# print(s.index('aug')) 
+
+# print(s.index(''))  
+
+# print(s[::-1])
+
+# print(s[6:-9])
+
+#  print(s[-1:-6:-2])
+
+
+
+# print(s[-10:-13:-1])
+
+# print(s[6:7])
+print(s[-9:-10:-1])
+
+
+# print(s[10:13:2])
+# print(s[-5:-8:-2])
+# print(s[-2:-4:-1])
+
+
+
+
