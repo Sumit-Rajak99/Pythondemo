eb95e28c95b197908605a1d7b8cefef07e19f44e
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-06 20:53:25 +0530

    numericdatatype

---
+++ numrecdatatype.py
@@ -0,0 +1,88 @@
+
+
+x,y = 10,-5
+print(x,y)
+print(type(x),type(y))
+
+# lenght
+# print(len(x))
+
+# max
+# print(max(x))
+
+# min
+# print(min(x))
+
+# type
+
+print(type(x))
+
+# id
+print(id(x))
+
+#print
+print(x)
+
+# sum
+
+# print(sum(x))
+
+
+x,y=10.5 ,-10.0
+
+print(x,y)
+
+print(type(x),type(y))
+
+
+# lenght
+
+# print(len(x))
+
+# max
+# print(max(x))
+
+# min
+# print(min(x))
+
+# type
+print(type(x))
+
+# id
+
+print(id(x))
+
+# sum
+# print(sum(x))
+
+
+
+# complex
+
+x,y=10.5+3j,-10-2j
+
+print(x,y)
+
+print(type(x),type(y))
+
+# lenght
+
+# print(len(x))
+
+# max
+# print(max(x))
+
+# min
+# print(min(x))
+
+# type
+print(type(x))
+
+# id
+
+print(id(x))
+
+# sum
+# print(sum(x))
+
+
