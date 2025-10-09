299e44b2f9b55f92ecf89169bd49c6c419349ec4
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-09 22:33:50 +0530

    list method

---
+++ listtype.py
@@ -0,0 +1,42 @@
+#List = collection of elemnts 
+# * elemnts are two types = 1.Homogeneroos (same data-type) 2. Hetrogenoos (diffrent data-type)
+
+# Represent by [] with commac(,) sepered elemnts
+# sequential/orded collection
+# indexing supported
+# slicing supported 
+# duplicates are allowed 
+# mutable in nature
+
+
+# mylist=[10,30,20,'python','java']
+# print(mylist,type(mylist))
+
+# inbuildfunction use in list 
+# len()
+# type()
+# id()
+# print()
+# max()
+# min()
+# sum()
+
+
+# mylist=[10,30,20,'python','java']
+# print(len(mylist))
+# print(type(mylist))
+# print(id(mylist))
+# print(mylist)
+
+
+
+
+
+
+
+mylist=[10,20,20.9,50,10+3j]
+
+
+print(sum(mylist))
+print((max(mylist)))
+print(min(mylist))
