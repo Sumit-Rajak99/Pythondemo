b47655e6167d9d76c9eedee04752e83fd5a69df5
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-09 22:35:58 +0530

    method defition list

---
+++ lismethod.py
@@ -0,0 +1,42 @@
+# copy()=create new object with same element
+# clear()=remove all elements from given list
+# append()=add single element at last position
+# extend()=add multiple elements at last position
+# pop()=remove index targeted element by -default index is -1 that by remove last element 
+# remove()=remove targeted element from list
+# index()=return index no. against given element 
+# count()=findout frequency /occurece of any element
+# sort()=arrange in assending order
+# reverse()=reverse out all element from list
+# insert()=add element in  targeted position
+
+
+
+# copy 
+# l=[2,4,6,'python','java']
+# l1=l.copy()
+# print(l,l1,id(l),id(l1))
+
+
+# # clear
+# l.clear()
+# print(l,id(l))
+# # del l remove object from del 
+# print(l)
+
+
+
+# s='python'
+
+# print(s)
+# del s
+# print(s)
+
+# append()
+l=[2,4,6,'python','java']
+l.append('php')
+print(l)
+
+l.append([1,2,3,4])
+print(l)
+
