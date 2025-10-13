acc987a8fae831c70d28f6fa2fa74b6481057fd4
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-13 22:15:40 +0530

    dictnary method

---
+++ dictonarymethod.py
@@ -0,0 +1,68 @@
+# method  in dictionary
+
+# copy / 
+
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+# d1=d.copy
+# print(d,d1)
+# print(id(d),id(d1))
+
+# # clear()
+# d.clear()
+# print(d)
+# print(id(d))
+
+# del d
+# print(d)
+
+
+
+
+
+
+
+# get()
+
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+
+# # synatx of get =dicr.get('key','defualt_value')
+
+# print(d.get('name'))
+# print(d.get('Name'))
+# print(d.get('Name','Guest'))
+# print(d.get('name','Guest'))
+
+
+
+
+# keys(/)
+
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+
+# print(d.keys())
+
+
+
+# values ()
+
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+
+# print(d.values())
+
+
+# items()
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+
+# print(d.items())
+
+
+# updATe()
+
+# d={'name':'sumit','age':19,'qualification':'m.Teach'}
+
+# d1={'x':10,'y':20,'age':20}
+
+# d.update(d1)
+
+# print(d)
+# print(d1)
