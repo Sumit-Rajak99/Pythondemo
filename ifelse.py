bed9d18c34e7018263dc4feb53c7035dec67ba78
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-10-28 21:27:12 +0530

    controlstatment

---
+++ ifelse.py
@@ -0,0 +1,38 @@
+# if= block exucuted when conditon is true
+
+# n=int(input("enter any number"))
+# if n>0:
+#     print(f'given number {n} is positive')
+# print("thank you for visit") 
+   
+   
+   
+   
+ 
+   
+#    if else = block must be exucuted
+#  else = is a depented keword , else body  when given condition false
+#  true = if block execited
+# false = else block execited
+
+
+# n=int(input("enter any number :"))
+# if n>0:
+#     print(f'given number is {n} positive')
+# else:
+#     print(f'given number is {n} negative')
+# print("thanks for vist")
+
+
+    
+# if elif/ /
+
+n=int(input("enter any number : "))
+
+if n>0:
+    print(f'given number is {n} positive')
+elif n<0:
+    print(f'given number is {n} negative')
+else:
+    print(f'given number is {n} Zero')        
+    
