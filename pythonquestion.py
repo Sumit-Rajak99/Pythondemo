56f7d4207ad717f1d509d1945653e9e386d3129d
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-02 20:30:12 +0530

    pyhtonquestion

---
+++ pythonquestion.py
@@ -0,0 +1,65 @@
+# 1. What is programming-language?
+# Ans = programming language a set of rules symbol and syntax           and  programming language used for interact with computer and and programming language used for build website , apps,operating system , games
+
+#  2.Explain Types of programming-languages?
+# Ans programming languages are two type 
+# 1.low level language = low level language that a computer can understand almost directly, or it requires very little translation into machine language.They are hard for humans to understand but very fast for the computer. 0and 1
+# 2. High level language=These are easier for humans to understand and use english word
+# They need a compiler or interpreter to convert into machine code. 
+# Like = python,java,c,c++
+
+# 3.Write difference between frontend and backend?
+# Ans = frontend means visible part of website and app what user see and use design and layout of website and app user direct interact with it and like image button and logo video is called frontend of website and apps and frondend run on browse
+# backend=The hidden part that runs behind the scenes — handles data, logic, and servers developers and  servers use it users don’t see it. Backend languages like python,java,c,c++,nodejs and etc and backend focus on managing data, storing information, and making everything work.
+
+# 4. What is python?
+# Ans = python is a popular programming language it was created by guido van rassum and released in 1991.
+# Python is free available to download and use,
+# Its source code is open for modification and distribucation , it is interpreter language executed line by line it is high level language easy to understand and closer to human language
+
+# 5. What are the advantages of python? 
+# Ans advantages of python are-
+# Easy to learn= simple and readable syntax similar to english
+# Huge library support = python comes with a standard library math,datetime ,os,system etc
+
+# Huge communication support= one of the largest programming in the world ,easy to find tutorial ,documentations and solutions on formula like stack overflow
+
+# No need to declare database= python is dynamically typed , no need to specify int float or string
+
+# Dynamically  typed language = Variable can hold data of different types at different times this makes coding flexible and faster 
+
+# Interacts with other language = python can work with java c++ and .net easily
+
+# 6. What are the limitations of python?
+# Ans limitations of python are -
+# Slower execution= python is slower than compiled language like c.c++, java  because it is interpreter language excited line by line 
+
+# Runtime errors =  because it is dynamically typed error may only appar while runtime  the program
+
+# Memory managment= python uses automatic garbage collection developers have use control over memory allocation compared to c,c++
+# Mobile app development limitation= python is rarely used for mobile application development 
+  
+
+# 7. Explain internal working of python
+#  Ans= Internal Working of Python
+# Source Code (.py)
+# You write your Python program in a .py file.
+# Compilation to Bytecode
+# The Python interpreter first compiles the code into bytecode, a low-level set of instructions stored in .pyc files
+
+
+# Python Virtual Machine (PVM)
+
+
+# The PVM (Python Virtual Machine) executes this bytecode line by line.
+
+
+# It converts the bytecode into machine code that the computer can understand.
+
+
+# Execution and Output
+
+
+# Finally, the machine executes the instructions, and you see the program output on your screen
+# Python code → Compiled to Bytecode → Executed by PVM → Output displayed
+
