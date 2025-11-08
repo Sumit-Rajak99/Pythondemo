eb74a8f22919581d160a4ce9e9fc5733e4f4b922
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-08 22:13:46 +0530

    file handling ceate write read

---
+++ filehandling.py
@@ -0,0 +1,31 @@
+# x
+# f=open('sidii.txt','x')
+# print(f.name)
+# print(f.mode)
+# print(f.writable())
+# print(f.readable())
+# print(f.encoding)
+# print(f.closed)
+
+
+
+
+# f=open('sidii1.txt','a')
+# print(f.name)
+# print(f.mode)
+# print(f.writable())
+# print(f.readable())
+# print(f.encoding)
+# print(f.closed)
+
+
+
+
+
+# f = open('sidii.txt')
+# print(f.name)
+# print(f.mode)
+# print(f.writable())
+# print(f.readable())
+# print(f.closed)
+# print(f.encoding)  
