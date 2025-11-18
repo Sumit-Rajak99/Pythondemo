16be438c306812cf40055ad4607b3b818130f9b4
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-18 20:31:28 +0530

    on the basic of arfument

---
+++ functionpra.py
@@ -0,0 +1,114 @@
+# nature number
+# def nature(n):
+#     for i in range(1,n+1):
+#         print(i)
+ 
+ 
+# x=int(input("enetr any number: "))        
+# nature(x)    
+
+
+
+# def even(n):
+#     for i in range(1,n+1):
+#         print(i*2)
+        
+# x=int(input("enter any number : "))
+# even(x)   
+
+# odd 
+# def odd(n):
+#     for i in range(2,n+1,2):
+#         print(i)
+        
+# x=int(input("enter any number : "))
+# odd(x)       
+
+
+# factorial / 
+
+# def fac(n):
+#     sum=1
+#     for i in range(1,n+1):
+#         sum=sum*i
+#         print(sum)
+            
+        
+# x=int(input("enter any number : "))
+# fac(x)       
+
+
+
+
+# type of user difine function 
+# on the basic of argumnet
+
+# 1 positonal argumnet
+# sysnatx 
+# def add(a,b,c):
+#     print(a+b+c)
+    
+# add(10,20,30) 
+
+
+# 2 defualt positional argumnet
+
+# def add(x=0,y=0,z=0):
+#     print(x+y+z)
+    
+# add(10,20,)      
+
+
+
+# varivaleb lenght positionla argument 
+# def add(*args):
+#     print(args)
+    
+# add(23,20,55666,7,67,76,76,76,76,77,7,664476,76)     
+
+
+
+# def add(*n):
+#     sum=0
+#     for i in n:
+#         sum=sum+i
+#         print("sum",sum)
+        
+# add(10)         
+
+
+# def add(a,b=0,*c):
+#     print(a)
+#     print(b)
+#     print(c)    
+    
+# add(10,30,5,5,656,5354,25,2)    
+
+
+# 4. keyword positional argumnet
+
+# def add(a,c,b):
+#     print(a)
+#     print(b)
+#     print(c)
+    
+# add(b=10,c=40,a=20,)    
+ 
+                  
+# defual positional argumnet 
+
+# def add(x=0,y=0,z=0):
+#     print(x)
+#     print(y)
+#     print(z)
+    
+# add(x=20,y=40,z=50)     
+
+
+
+  
+
+
+
+
+               
