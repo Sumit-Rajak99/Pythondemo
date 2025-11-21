956e943a1ca4d9fdb16df3926337cb18b4a5b2d7
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-21 19:53:03 +0530

    if else question

---
+++ praticequestion.py
@@ -0,0 +1,62 @@
+# num=float(input("enter any number: "))
+# if num>0:
+#     print("the number is positive ")
+# elif num<0:
+#     print("the number is negative")
+    
+# else:
+#     print("the number is zero")        
+        
+         
+
+# x=int(input("enter numner1: "))
+# y=int(input("enetr number2: "))
+# z=int(input("enter number3: "))
+
+# if x>y and x>z:
+#     print("x is greater")
+# elif y>x and y>z:
+#     print("y is greter")
+# else:
+#     print("z is greter")   
+
+
+
+
+# age=int(input("enter your age: "))
+# if age>=18:
+#     print("you are elegble for vote")
+    
+# else:
+#     print("you are not elegible for vote:")              
+
+
+
+
+
+# year=int(input("entr any years: "))
+# if(year%4==0 and year%400!=0)or (year%100==0):
+#     print("leap year")
+# else:
+#     print("not leap years") 
+
+
+
+# marks=   
+
+# x=int(input("enetr any numer frpom 0 to 4: "))
+# if x==0:
+#     print("you entered",x)
+# elif x==1:
+#     print("you enrtered",x)
+# elif x==2:
+#     print("you enrtered",x)
+# elif x==3:
+#     print("you enrtered",x) 
+# elif x==4:
+#     print("you enrtered",x) 
+# else:
+#     print("invalid  input")  
+
+
+                
