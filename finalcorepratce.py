d25c1da94ff09cf748ce04bcdc8c65cc8093b25f
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-25 20:45:31 +0530

    set list tupple dict

---
+++ finalcorepratce.py
@@ -0,0 +1,62 @@
+# # l=[1,2,3,44,5,44,5]
+
+
+# # print(l.count(5))
+# # print(l.index(5))
+# # l.append(10)
+# # l2=l
+# # print(l2)
+# # l.sort()
+# # l2=l 
+# # print(l2)
+# # l.remove(5)
+# # l2=l
+# # print(l2)
+# # l2=l.append(10)
+# # print(l2)
+
+# # print(l)
+# # print(l.pop(1))
+# # print(l)
+# # print(id(l))
+# # l2=l.copy()
+# # print(l2)
+# # print(id(l2))
+
+# # l.insert(1,2)
+# # l2=l
+# # print(l2)
+
+# # l1=[1,2,3,3,4,55,4]
+# # l2=[1,2,3,3,4,4,3,3,2,5]
+# # print(l1%l2)
+
+# # tupples
+
+
+# t=(342,45,35,647,75,785,7,3,7)
+# # print(max(t))
+# # print(t.count(7))
+# # print(t.index(7))
+# # print(t.)
+
+
+
+
+
+# dict 
+
+
+d={"name":"sumit",
+   "age":19,
+   "course":"fullsatck",
+   
+   }
+# print(len(d))
+# print(d.keys())
+# print(d.get("name"))
+# print(d.values())
+# d1=d.items()
+# print(type(d1))
+
+print(d.fromkeys("age"))
---
+++ testcore.py
@@ -0,0 +1,18 @@
+# l=[10,20,"java"]
+# fs=frozenset(l)
+# print(fs)
+# print(id(fs))
+
+
+l=[10,20,"java"]
+fs=frozenset(l)
+
+j=[34,2,4,43,"java"]
+fs1=frozenset(j)
+# print(fs1,fs)
+
+# print(fs1.difference(fs))
+# print(fs.intersection(fs1))/
+# print(fs.issubset(fs1))
+# print(fs.issuperset(fs1))
+# print(fs.isdisjoint(fs1))
